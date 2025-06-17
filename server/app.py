f3d972c050d8f313e644ebf91554d3ee19ac50e1
Cristina <kamuthucristina@gmail.com>
2025-06-17 15:34:54 +0300

    initialized the app.py

---
+++ server/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, jsonify, make_response
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+from models import db, User, Review, Game
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///pizza.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.json.compact = False
+
+db.init_app(app)
