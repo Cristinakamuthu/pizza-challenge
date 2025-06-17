d564ee59815b9d5501dc219f1bd917bef4267ffe
Cristina <kamuthucristina@gmail.com>
2025-06-17 16:17:24 +0300

    created my first route

--- server/app.py
+++ server/app.py
@@ -11,3 +11,18 @@ app.json.compact = False
 
 db.init_app(app)
 
+@app.route('/restaurants')
+def get_restaurants():
+    restaurants = Restaurant.query.all()
+
+    restaurant_list = [r.to_dict() for r in restaurants]
+
+    response = make_response(jsonify(restaurant_list), 200)
+
+    return response
+
+if __name__ =='__main__':
+    app.run(port=5555, debug=True)
+  
+
+
